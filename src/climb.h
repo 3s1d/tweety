90424f3f66574c66ed6779e1d90233ec12b5ebab
Juergen Eckert <juergen@familie-eckert.de>
2016-08-02 16:50:08 +0200

    comment added

--- src/climb.h
+++ src/climb.h
@@ -24,8 +24,10 @@
 
 #ifdef CLIMB_TEST
 
+/*adjust test values here*/
 #define C_START_ALT_CM			200000
 #define	C_CLIMB_CMS			200			//resolution is CLIMB_SAMPLES_PER_SEC
+/******/
 
 #define C_ADD				C_CLIMB_CMS/CLIMB_SAMPLES_PER_SEC
 
