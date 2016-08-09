4ad6822b4112116ddf1ad7282daa2a196b7b32f9
Juergen Eckert <juergen@familie-eckert.de>
2016-08-09 10:56:13 +0200

    added missinf file

---
+++ src/main.h
@@ -0,0 +1,14 @@
+/*
+ * main.h
+ *
+ *  Created on: Aug 8, 2016
+ *      Author: sid
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#define MAINLOOP_TICKSPERMINUTE		356
+#define MAINLOOP_HALFHOUR			MAINLOOP_TICKSPERMINUTE * 30
+
+#endif /* MAIN_H_ */
